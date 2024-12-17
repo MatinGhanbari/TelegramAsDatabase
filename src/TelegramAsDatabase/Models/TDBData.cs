21f3e0b6001259d49eadd2d5d167620a2de618d0
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-17 15:36:25 +0330

    Add TDBData class

---
+++ src/TelegramAsDatabase/Models/TDBData.cs
@@ -0,0 +1,15 @@
+﻿using MemoryPack;
+using Newtonsoft.Json;
+
+namespace TelegramAsDatabase.Models;
+
+[Serializable]
+[MemoryPackable]
+public partial class TDBData<T>
+{
+    public Guid Id { get; set; }
+    public T Data { get; set; }
+
+    public static implicit operator string(TDBData<T> data) => JsonConvert.SerializeObject(data)!;
+    public static implicit operator TDBData<T>(string data) => JsonConvert.DeserializeObject<TDBData<T>>(data)!;
+}
