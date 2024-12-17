ba781e34d9d5713f63941ad1530b16851d0d48b4
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-17 15:37:52 +0330

    Add TDB functional test

---
+++ test/Test.Functional/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TelegramAsDatabase.ConfigurationServices;
+using Test.Functional;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((hostingContext, configuration) =>
+            {
+                configuration.AddJsonFile(path: "appsettings.json", optional: false, true);
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                var configuration = hostContext.Configuration;
+
+                services.AddHostedService<HostedService>();
+
+                // Add other services
+                services.AddTDB(configuration);
+            });
+}
