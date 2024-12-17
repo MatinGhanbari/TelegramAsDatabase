4720aa6d530a853afcebb63f60beef17fc2e7c96
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-17 15:37:10 +0330

    Add ITDB contract

---
+++ src/TelegramAsDatabase/Contracts/ITDB.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using TelegramAsDatabase.Models;
+
+namespace TelegramAsDatabase.Contracts;
+
+public interface ITDB
+{
+    Task<Result<TDBData<T>>> GetAsync<T>(Guid id, CancellationToken cancellationToken = default);
+    Task<Result<bool>> ExistsAsync<T>(Guid id, CancellationToken cancellationToken = default);
+    Task<Result> SaveAsync<T>(TDBData<T> data, CancellationToken cancellationToken = default);
+    Task<Result> ClearAsync(CancellationToken cancellationToken = default);
+}
