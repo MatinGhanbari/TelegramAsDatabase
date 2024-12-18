c44f16d7443410803cec5840fe794b5c4bec4da2
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-19 01:43:02 +0330

    Add Constants

---
+++ src/TelegramAsDatabase/Constants/TelegramApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramAsDatabase.Constants;
+
+internal static class TelegramApiConstants
+{
+    internal const int MaxGroupMessagesPerMinute = 20;
+    internal const int MaxChatMessagesPerSecond = 1;
+}
---
+++ src/TelegramAsDatabase/Constants/TelegramMessageConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramAsDatabase.Constants;
+
+internal static class TelegramMessageConstants
+{
+    internal const int MaxMessageLength = 4096;
+}
