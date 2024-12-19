f26ba0836e4fa50cfcdc2e7637ef139efc666a3d
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-19 15:59:27 +0330

    Update TDBConstants

--- src/TelegramAsDatabase/TDBConstants.cs
+++ src/TelegramAsDatabase/TDBConstants.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TelegramAsDatabase.Constants;
+namespace TelegramAsDatabase;
 
-internal static class TelegramApiConstants
+internal class TDBConstants
 {
+    internal const int MaxMessageLength = 4096;
+
     internal const int MaxGroupMessagesPerMinute = 20;
     internal const int MaxChatMessagesPerSecond = 1;
 }
