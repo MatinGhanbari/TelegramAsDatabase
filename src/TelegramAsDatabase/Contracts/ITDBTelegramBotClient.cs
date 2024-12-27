5bca4d59281f46667f815a56d9f52ee5cfd963d5
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-12-27 17:38:41 +0330

    Fix message length limit issue #5

---
+++ src/TelegramAsDatabase/Contracts/ITDBTelegramBotClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramAsDatabase.Contracts;
+
+public interface ITDBTelegramBotClient : ITelegramBotClient
+{
+    Task<Message> SaveAsync(
+       ChatId chatId,
+       string text,
+       ParseMode parseMode = ParseMode.None,            
+       ReplyParameters? replyParameters = null,
+       IReplyMarkup? replyMarkup = null,
+       LinkPreviewOptions? linkPreviewOptions = null,
+       int? messageThreadId = null,
+       IEnumerable<MessageEntity>? entities = null,
+       bool disableNotification = false,
+       bool protectContent = false,
+       string? messageEffectId = null,
+       string? businessConnectionId = null,
+       bool allowPaidBroadcast = false,
+       CancellationToken cancellationToken = default);
+
+    Task<Message> UpdateAsync(
+        ChatId chatId,
+        int messageId,
+        string text,
+        ParseMode parseMode = ParseMode.None,
+        IEnumerable<MessageEntity>? entities = null,
+        LinkPreviewOptions? linkPreviewOptions = null,
+        InlineKeyboardMarkup? replyMarkup = null,
+        string? businessConnectionId = null,
+        CancellationToken cancellationToken = default(CancellationToken));
+
+    Task<string> GetAsync(
+        ChatId chatId,
+        int messageId,
+        CancellationToken cancellationToken = default);
+}
--- src/TelegramAsDatabase/Implementations/TDBTelegramBotClient.cs
+++ src/TelegramAsDatabase/Implementations/TDBTelegramBotClient.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
 using Polly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Requests.Abstractions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 using TelegramAsDatabase.Configs;
+using TelegramAsDatabase.Contracts;
 
 namespace TelegramAsDatabase.Implementations;
 
-internal class TDBTelegramBotClient : TelegramBotClient
+public class TDBTelegramBotClient : TelegramBotClient, ITDBTelegramBotClient
 {
     private readonly ILogger<TDBTelegramBotClient> _logger;
     private readonly TDBConfig _config;
@@ -34,5 +40,71 @@ internal class TDBTelegramBotClient : TelegramBotClient
             );
 
         return await policy.ExecuteAsync(async () => await base.SendRequest(request, cancellationToken));
+    }
+
+    public async Task<Message> SaveAsync(
+     ChatId chatId,
+     string text,
+     ParseMode parseMode = ParseMode.None,
+     ReplyParameters? replyParameters = null,
+     IReplyMarkup? replyMarkup = null,
+     LinkPreviewOptions? linkPreviewOptions = null,
+     int? messageThreadId = null,
+     IEnumerable<MessageEntity>? entities = null,
+     bool disableNotification = false,
+     bool protectContent = false,
+     string? messageEffectId = null,
+     string? businessConnectionId = null,
+     bool allowPaidBroadcast = false,
+     CancellationToken cancellationToken = default)
+    {
+        if (text.Length < TDBConstants.MaxMessageLength)
+        {
+            return await this.SendMessage(chatId, text, parseMode, replyParameters,
+                replyMarkup, linkPreviewOptions, messageThreadId, entities,
+                disableNotification, protectContent,
+                messageEffectId, businessConnectionId,
+                allowPaidBroadcast, cancellationToken);
+        }
+
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(text));
+        var document = InputFile.FromStream(stream);
+
+        return await this.SendDocument(chatId, document, null, parseMode, replyParameters, replyMarkup, null, messageThreadId, null, false, disableNotification, false, messageEffectId, businessConnectionId, allowPaidBroadcast, cancellationToken);
+    }
+
+    public async Task<Message> UpdateAsync(ChatId chatId, int messageId, string text,
+        ParseMode parseMode = ParseMode.None, IEnumerable<MessageEntity>? entities = null, LinkPreviewOptions? linkPreviewOptions = null,
+        InlineKeyboardMarkup? replyMarkup = null, string? businessConnectionId = null,
+        CancellationToken cancellationToken = default)
+    {
+        Task.Run(async () => await this.DeleteMessage(_config.ChannelId, messageId, cancellationToken), cancellationToken);
+        return await SaveAsync(chatId, text, parseMode, null, replyMarkup, linkPreviewOptions, cancellationToken: cancellationToken);
+    }
+
+    public async Task<string> GetAsync(
+     ChatId chatId,
+     int messageId,
+     CancellationToken cancellationToken = default)
+    {
+        var message = await this.ForwardMessage(chatId, _config.ChannelId, messageId, cancellationToken: cancellationToken);
+        Task.Run(async () => await this.DeleteMessage(_config.ChannelId, message.MessageId, cancellationToken), cancellationToken);
+
+        if (message.Type == MessageType.Text)
+        {
+            return message.Text;
+        }
+
+        if (message.Type == MessageType.Document)
+        {
+            var stream = new MemoryStream();
+            var fileName = message.Document.FileName;
+            var fileInfo = await this.GetInfoAndDownloadFile(message.Document.FileId, stream, cancellationToken);
+
+            var buffer = stream.ToArray();
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        throw new Exception("Unexpected message");
     }
 }
